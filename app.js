0b0395f6991c099a7fca852c061b9fe974ae9ba5
Twan Box <twanbox@gmail.com>
2020-08-31 10:04:49 +0200

    setup google oauth userAuth

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -12,7 +17,19 @@ const app = express();
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const port = process.env.PORT;
 
