48b3481054a1624055868b1d6bee35a032873953
Twan Box <twanbox@gmail.com>
2020-09-01 22:25:22 +0200

    users can create a new story - their stories get displayed on their dashboard for now

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
@@ -14,6 +16,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
@@ -21,6 +26,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
 app.use(passport.initialize());
@@ -30,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const port = process.env.PORT;
 
